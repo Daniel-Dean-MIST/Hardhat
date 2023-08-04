3c594103e3df5517ac64b5110a50b3f9ee15ed41
Daniel_Dean <danieldean.mist@gmail.com>
2023-08-04 14:18:43 -0400

    Added withdrawal, levelUp and setLevelUpFee fun

--- hardhat-tutorial/contracts/TroopHelper.sol
+++ hardhat-tutorial/contracts/TroopHelper.sol
@@ -6,6 +6,8 @@ import "./TroopAttack.sol";
 
 contract TroopHelper is TroopAttack{
 
+    uint levelUpFee = 0.001 ether;
+
     modifier canLevelUp(uint _troopId) {
         require(troops[_troopId].isLevelable == true);
         require(troops[_troopId].troopLevel >= 1);
@@ -13,6 +15,20 @@ contract TroopHelper is TroopAttack{
         Troop storage troop = troops[_troopId];
         _;
     }//aboveLevel
+
+    function withdraw() external onlyOwner {
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
+    }//withdraw
+
+    function levelUp(uint _troopId) external payable {
+        require(msg.value == levelUpFee);
+        troops[_troopId].troopLevel++;
+    }//levelUp
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }//setLevelUpFee
 
     //function increaseStats(uint _troopId) external canLevelUp(_troopId) {
     //    troops[_troopId].troopStrength++;
